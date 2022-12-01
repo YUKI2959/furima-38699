40d614cc43e218baac69d6ba36eff70b1d09486f
内田有貴 <117145436+YUKI2959@users.noreply.github.com>
2022-12-01 18:20:55 +0900

    バリデーションを記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # presence: true 空を許可しない
+  validates :birthday, presence: true
+
+  # presence: true 空を許可しない
+  with_options presence: true do
+    # ひらがな、漢字のみ許可する
+    validates :last_name,:first_name, presence: true,format: {with: /\A[ぁ-ん一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    # カタカナのみ許可する
+    validates :last_name_kana,:first_name_kana, presence: true,format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+  end
 end
