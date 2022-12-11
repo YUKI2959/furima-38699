e6510609e46332ff89aab398254583295fbb30bb
内田有貴 <117145436+YUKI2959@users.noreply.github.com>
2022-12-11 11:33:40 +0900

    orderから2つのテーブルに保存するよう設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,5 +8,18 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.create(order_params)
+    OrderInformation.create(order_information_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def order_params
+    params.permit(:item_id).merge(user_id: current_user.id)
+  end
+
+  def order_information_params
+    params.permit(:post_code, :area_id, :municipality, :address, :building_name, :phone_number).merge(order_id: @order.id)
   end
 end
