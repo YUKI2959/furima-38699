cbc3b324748c9ce2e04b147ac81b57de3769af54
内田有貴 <117145436+YUKI2959@users.noreply.github.com>
2022-12-11 13:11:15 +0900

    バリデーション設定

--- app/models/order_information.rb
+++ app/models/order_information.rb
@@ -1,3 +1,11 @@
 class OrderInformation < ApplicationRecord
   belongs_to :order
+
+  # presence: true 空を許可しない
+  # 10桁以上11桁以内の半角数値
+  validates :post_code, presence: true, format: {with: /\A\d{10,11}\z/ , message: "is invalid. 10 to 11 digit half-width numeric value"}
+
+  # presence: true 空を許可しない
+  validates :order, :post_code, :municipality, :address, :phone_number, presence: true
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
 end
