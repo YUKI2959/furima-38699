4a31ce62457ec4ca2e822054ce2930a1281f1616
内田有貴 <117145436+YUKI2959@users.noreply.github.com>
2022-12-13 09:58:11 +0900

    バリデーション修正+リファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,18 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_root_path, only: [:index]
 
   def index
-    @order_form = OrderForm.new
-    @item = Item.find(params[:item_id])
+    if @item.order == nil && @item.user_id != current_user.id && user_signed_in?
+      @order_form = OrderForm.new
+    else
+      redirect_to root_path
+    end
   end
 
   def create
     @order_form = OrderForm.new(order_params)
-    @item = Item.find(params[:item_id])
     if @order_form.valid?
       pay_item
       @order_form.save
@@ -18,10 +22,21 @@ class OrdersController < ApplicationController
     end
   end
 
+
   private
 
   def order_params
     params.require(:order_form).permit(:post_code, :area_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id] )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_root_path
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 
   def pay_item
