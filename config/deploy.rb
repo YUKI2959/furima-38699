de3078ef0a95e65ec11b3163cefcf1310e770bab
内田有貴 <117145436+YUKI2959@users.noreply.github.com>
2022-12-17 13:21:42 +0900

    自動デプロイ準備(修正1)

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.17.1'
 set :application, 'furima-38699'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:Githubのユーザー名/furima-38699.git'
+set :repo_url,  'git@github.com:YUKI2959/furima-38699.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
